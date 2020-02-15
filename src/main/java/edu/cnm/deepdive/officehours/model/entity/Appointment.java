caac694d6e83dd4b75495ca7e5cc2fefa05b6004
Jose Ortiz <jose.ortiz@live.com>
2020-02-15 16:38:41 -0700

    Entities User and Appointment started.

--- src/main/java/edu/cnm/deepdive/officehours/model/entity/Appointment.java
+++ src/main/java/edu/cnm/deepdive/officehours/model/entity/Appointment.java
@@ -1,8 +1,109 @@
 package edu.cnm.deepdive.officehours.model.entity;
 
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.NonNull;
 
 @Entity
 public class Appointment {
 
+  @NonNull
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column( name = "appointment_id", columnDefinition = "CHAR(16) FOR BIT DATA",
+        nullable = false, updatable = false)
+  private UUID id;
+
+
+//  @NonNull
+//  @OneToOne ( cascade = {CascadeType.DETACH, CascadeType.MERGE,
+//      CascadeType.PERSIST, CascadeType.REFRESH})
+//  @JoinColumn(name = "student_id", referencedColumnName = ap)
+//  private Set<Student> studentId;
+
+  @NonNull
+  private UUID teacherId;
+
+
+  @NonNull
+  @Column(nullable = false)
+  private String status;
+
+  @NonNull
+
+  private Date start;
+
+  @NonNull
+  private Date end;
+
+  @NonNull
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false, updatable = false)
+  private Date created;
+
+  @NonNull
+  @UpdateTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(nullable = false)
+  private Date updated;
+
+
+  public void setStatus(@NonNull String status) {
+    this.status = status;
+  }
+
+  @NonNull
+  public Date getEnd() {
+    return end;
+  }
+
+  @NonNull
+  public UUID getId() {
+    return id;
+  }
+
+  @NonNull
+  public UUID getStudentId() {
+    return studentId;
+  }
+
+  @NonNull
+  public UUID getTeacherId() {
+    return teacherId;
+  }
+
+  @NonNull
+  public String getStatus() {
+    return status;
+  }
+
+  @NonNull
+  public Date getStart() {
+    return start;
+  }
+
+  @NonNull
+  public Date getCreated() {
+    return created;
+  }
+
+  @NonNull
+  public Date getUpdated() {
+    return updated;
+  }
 }
--- src/main/java/edu/cnm/deepdive/officehours/model/entity/User.java
+++ src/main/java/edu/cnm/deepdive/officehours/model/entity/User.java
@@ -1,8 +1,62 @@
 package edu.cnm.deepdive.officehours.model.entity;
 
+import java.util.UUID;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.lang.NonNull;
 
 @Entity
 public class User {
 
+  @NonNull
+  @Id
+  @GeneratedValue(generator =  "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column( name = "user_id", columnDefinition = "CHAR(16) FOR BIT DATA",
+  nullable = false, updatable = false)
+  private UUID id;
+
+  @NonNull
+  @Column(length = 4696, nullable = false, unique = true)
+  private String oauth;
+
+  private String nickname;
+
+  @NonNull
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @NonNull
+  public UUID getId() {
+    return id;
+  }
+
+  @NonNull
+  public String getOauth() {
+    return oauth;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  @NonNull
+  public String getEmail() {
+    return email;
+  }
+
+  public void setOauth(@NonNull String oauth) {
+    this.oauth = oauth;
+  }
+
+  public void setNickname(String nickname) {
+    this.nickname = nickname;
+  }
+
+  public void setEmail(@NonNull String email) {
+    this.email = email;
+  }
 }
