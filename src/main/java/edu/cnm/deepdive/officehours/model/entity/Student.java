5db30881b81757dfbcb9b028eee326a267020886
Alexx <Vexurion@gmail.com>
2020-02-15 16:40:26 -0700

    Entities Student and Teacher started.

--- src/main/java/edu/cnm/deepdive/officehours/model/entity/Student.java
+++ src/main/java/edu/cnm/deepdive/officehours/model/entity/Student.java
@@ -1,8 +1,33 @@
 package edu.cnm.deepdive.officehours.model.entity;
 
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.lang.NonNull;
 
 @Entity
 public class Student {
+
+  @NonNull
+  @Id
+  @GeneratedValue(generator = "uuid2")
+  @GenericGenerator(name = "uuid2", strategy = "uuid2")
+  @Column(name = "student_id", columnDefinition = "CHAR(16) FOR BIT DATA",
+      nullable = false, updatable = false)
+  private UUID id;
+
+  @NonNull
+  @OneToOne(
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH }
+      )
+  @Column(nullable = false, updatable = false)
+  private User user_id;
 
 }
--- src/main/java/edu/cnm/deepdive/officehours/model/entity/Teacher.java
+++ src/main/java/edu/cnm/deepdive/officehours/model/entity/Teacher.java
@@ -1,8 +1,31 @@
 package edu.cnm.deepdive.officehours.model.entity;
 
+import java.util.UUID;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.lang.NonNull;
 
 @Entity
 public class Teacher {
+
+  @NonNull
+  @Id
+  @GeneratedValue(generator = "uuid3")
+  @GenericGenerator(name = "uuid3", strategy = "uuid3")
+  @Column(name = "teacher_id", columnDefinition = "CHAR(16) FOR BIT DATA",
+      nullable = false, updatable = false)
+  private UUID id;
+
+  @NonNull
+  @OneToOne(
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH }
+  )
+  @Column(nullable = false, updatable = false)
+  private User user_id;
 
 }
