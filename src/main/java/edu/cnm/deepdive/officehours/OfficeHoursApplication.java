76f583197eec7c4b6ca52f2db123280cebba5929
Jose Ortiz <jose.ortiz@live.com>
2020-02-15 15:33:08 -0700

    Initial Commit.

---
+++ src/main/java/edu/cnm/deepdive/officehours/OfficeHoursApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.officehours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeHoursApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OfficeHoursApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/officehours/OfficeHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.officehours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeHoursApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
