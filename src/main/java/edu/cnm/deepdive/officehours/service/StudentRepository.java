6ef267acb888006085700266513ff2a978e91b71
Alexx <Vexurion@gmail.com>
2020-02-27 14:55:48 -0700

    Student Repo

--- src/main/java/edu/cnm/deepdive/officehours/service/StudentRepository.java
+++ src/main/java/edu/cnm/deepdive/officehours/service/StudentRepository.java
@@ -1,5 +1,16 @@
 package edu.cnm.deepdive.officehours.service;
 
-public interface StudentRepository {
+import edu.cnm.deepdive.officehours.model.entity.Student;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+
+  Iterable<Student> getAllByOrderById();
+
+  Iterable<Student> getAllByNameContainsOrderByNameAsc(String Name);
+
+  default Student findOrFail(UUID id) {
+    return findById(id).get();
+  }
 }
