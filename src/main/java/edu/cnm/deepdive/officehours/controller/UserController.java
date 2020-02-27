75e52d2c3d82fce8082ba10348a2e01f7fa5ae27
Alexx <Vexurion@gmail.com>
2020-02-27 13:37:25 -0700

    Controller

---
+++ src/main/java/edu/cnm/deepdive/officehours/controller/UserController.java
@@ -0,0 +1,58 @@
+package edu.cnm.deepdive.officehours.controller;
+
+import edu.cnm.deepdive.officehours.model.entity.User;
+import java.util.Set;
+import java.util.UUID;
+import org.springframework.hateoas.server.ExposesResourceFor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/User")
+@ExposesResourceFor(User.class)
+public class UserController {
+
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<User> post(@RequestBody User user) {
+    repository.save(user);
+    return ResponseEntity.created(user.getEmail()).body(user);
+  })
+
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public User get(@PathVariable UUID id) {
+    return repository.findById(id).get();
+  }
+
+  @DeleteMapping(value = "/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable UUID id) {
+    repository.findById(id).ifPresent((user) -> {
+      Set<User> students = user.getEmail();
+      user.forEach((User) -> quote.setSource(null));
+      students.clear();
+      repository.delete(user);
+    });
+  }
+
+
+  @PutMapping(value = "/{id}",
+      consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+  public User put(@PathVariable UUID id, @RequestBody User updated) {
+    User user = get(id);
+    user.setEmail(updated.getEmail());
+    return repository.save(user);
+  }
+
+
+}
