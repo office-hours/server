26410fddd5e73b53c7a7b394f4e80955ddab2186
Alexx <Vexurion@gmail.com>
2020-03-04 15:39:09 -0700

    Modified Status Enum and StatusConverter

--- src/main/java/edu/cnm/deepdive/officehours/Status.java
+++ src/main/java/edu/cnm/deepdive/officehours/Status.java
@@ -1,10 +1,19 @@
 package edu.cnm.deepdive.officehours;
 
 public enum Status {
-  NO_SHOW,
-  PENDING,
-  LATE,
-  CANCELLED,
-  ARRIVED;
-  ;
+  NO_SHOW("N"),
+  PENDING("P"),
+  LATE("L"),
+  CANCELLED("X"),
+  ARRIVED("K");
+
+  private String code;
+
+  private Status(String code) {
+    this.code = code;
+  }
+
+  public String getCode() {
+    return code;
+  }
 }
