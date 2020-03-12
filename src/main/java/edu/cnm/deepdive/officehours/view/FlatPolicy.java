14f3c92b7f28341abcf11e86374231d6866224bf
Alexx <Vexurion@gmail.com>
2020-03-12 00:57:49 -0600

    FlatPolicy docs

--- src/main/java/edu/cnm/deepdive/officehours/view/FlatPolicy.java
+++ src/main/java/edu/cnm/deepdive/officehours/view/FlatPolicy.java
@@ -7,21 +7,53 @@ import org.springframework.lang.NonNull;
 
 @JsonPropertyOrder({"id", "created", "updated", "starAvailable", "endAvailable", "blockTime"})
 public interface FlatPolicy {
+  /**
+   * returns the Id (UUID) of a policy resource.
+   *
+   * @return policy UUID
+   */
   @NonNull
   UUID getId();
 
+  /**
+   * Returns the date-time stamp recorded when a policy resource is first written to the database.
+   *
+   * @return creation timestamp
+   */
   @NonNull
   Date getCreated();
 
+  /**
+   * Returns the date-time stamp recorded when a policy resource is updated in the database.
+   *
+   * @return updated timestamp
+   */
+
   @NonNull
   Date getUpdated();
+  /**
+   * returns an available start time policy for teacher in appointment.
+   *
+   * @return policy available start date
+   */
 
   @NonNull
   Date getStartAvailable();
 
+  /**
+   * returns an available end time policy for teacher in appointment.
+   *
+   * @return policy available end date.
+   */
   @NonNull
   Date getEndAvailable();
 
+
+  /**
+   * returns an available block time policy for teacher in appointment.
+   *
+   * @return policy available block-time.
+   */
   @NonNull
   Date getBlockTime();
 }
